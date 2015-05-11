2065e91616b9274275f18294c7c674a0cff7dfbb
Michael <mhardy17@bayschoolsf.org>
2015-05-10 22:17:22 -0700

    added eyebrows simplified ui

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 //There are 155,520 unique monsters
 
-var hats = ['', '&nbsp;&nbsp;&nbsp;_====_</br>', '&nbsp;&nbsp;&nbsp;_____</br>&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</br>&nbsp;_|_____|_</br>', '&nbsp;&nbsp;&nbsp;_____</br>&nbsp;_(_____)_</br>'];
+var hats = ['', '&nbsp;&nbsp;_=====_</br>', '&nbsp;&nbsp;&nbsp;_____</br>&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</br>&nbsp;_|_____|_</br>', '&nbsp;&nbsp;&nbsp;_____</br>&nbsp;_(_____)_</br>'];
 var necks = ['\\&nbsp;/', false];
 var nJoins = ['-&nbsp;⁀&nbsp;-', false];
-var eyes = ['0', 'O', '*', '.', '^', '@'];
+var eyeBs = ['⁔', '^']
+var eyes = ['0', '*', '.', '^', '@'];
 var arms = ['\\', '/', '&nbsp;'];
 var rArms = ['/', '\\', '&nbsp;'];
 var feet = ['("")&nbsp;("")', '(^^)&nbsp;(^^)', '(vv)&nbsp;(vv)', '(_______)', ''];
-var mouths = ['‿', '_', '⁔', '*', '-', '&nbsp;'];
+var mouths = ['‿', '_', '⁔', '*', '&nbsp;'];
 
 var hat = hats[0];
 var neck = necks[0];
 var nJoin = nJoins[0];
+var lEyeB = eyeBs[0];
+var rEyeB = eyeBs[0];
 var lEye = eyes[0];
 var rEye = eyes[0];
 var lArm = arms[0];
@@ -21,13 +24,13 @@ var mouth = mouths[0];
 
 function gMonster(){
     if (nJoin && neck){
-        monster = hat+'&nbsp;&nbsp;&nbsp;⁔&nbsp;&nbsp;⁔</br>&nbsp;&nbsp;('+lEye+')&nbsp;('+rEye+')</br>&nbsp;&nbsp;&nbsp;&nbsp;'+neck+'</br>&nbsp;'+lArm+'&nbsp;'+nJoin+'&nbsp;'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
+        monster = hat+'&nbsp;&nbsp;&nbsp;'+lEyeB+'&nbsp;&nbsp;&nbsp;'+rEyeB+'</br>&nbsp;&nbsp;('+lEye+')&nbsp;('+rEye+')</br>&nbsp;&nbsp;&nbsp;&nbsp;'+neck+'</br>&nbsp;'+lArm+'&nbsp;'+nJoin+'&nbsp;'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
     }else if (!(nJoin) && neck){
-        monster = hat+'&nbsp;&nbsp;&nbsp;⁔&nbsp;&nbsp;⁔</br>&nbsp;&nbsp;('+lEye+')&nbsp;('+rEye+')</br>&nbsp;'+lArm+'&nbsp;&nbsp;'+neck+'&nbsp;&nbsp;'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
+        monster = hat+'&nbsp;&nbsp;&nbsp;'+lEyeB+'&nbsp;&nbsp;'+rEyeB+'</br>&nbsp;&nbsp;('+lEye+')&nbsp;('+rEye+')</br>&nbsp;'+lArm+'&nbsp;&nbsp;'+neck+'&nbsp;&nbsp;'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
     }else if (nJoin && !(neck)){
-        monster = hat+'&nbsp;&nbsp;&nbsp;⁔&nbsp;&nbsp;⁔</br>&nbsp;&nbsp;('+lEye+')&nbsp;('+rEye+')</br>&nbsp;'+lArm+'&nbsp;'+nJoin+'&nbsp;'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
+        monster = hat+'&nbsp;&nbsp;&nbsp;'+lEyeB+'&nbsp;&nbsp;'+rEyeB+'</br>&nbsp;&nbsp;('+lEye+')&nbsp;('+rEye+')</br>&nbsp;'+lArm+'&nbsp;'+nJoin+'&nbsp;'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
     }else{
-        monster = hat+'&nbsp;&nbsp;&nbsp;⁔&nbsp;&nbsp;⁔</br>&nbsp;'+lArm+'('+lEye+')&nbsp;('+rEye+')'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
+        monster = hat+'&nbsp;&nbsp;&nbsp;'+lEyeB+'&nbsp;&nbsp;'+rEyeB+'</br>&nbsp;'+lArm+'('+lEye+')&nbsp;('+rEye+')'+rArm+'</br>&nbsp;&nbsp;(&nbsp;&nbsp;'+mouth+'&nbsp;&nbsp;)</br>&nbsp;'+foot;
     };
     return(monster);
 };
--- ui.js
+++ ui.js
@@ -5,6 +5,10 @@ function uMonster(){
     lEye = eyes[loopCheck(radio)];
     var radio = document.getElementsByName('re');
     rEye = eyes[loopCheck(radio)];
+    var radio = document.getElementsByName('lb');
+    lEyeB = eyeBs[loopCheck(radio)];
+    var radio = document.getElementsByName('rb');
+    rEyeB = eyeBs[loopCheck(radio)];
     var radio = document.getElementsByName('n');
     neck = necks[loopCheck(radio)];
     var radio = document.getElementsByName('j');
@@ -21,10 +25,51 @@ function uMonster(){
     document.getElementById('m').innerHTML = m;
 };
 
+function sUMonster(){
+    var radio = document.getElementsByName('h');
+    hat = hats[loopCheck(radio)];
+    var radio = document.getElementsByName('e');
+    lEye = eyes[loopCheck(radio)];
+    rEye = eyes[loopCheck(radio)];
+    var radio = document.getElementsByName('b');
+    lEyeB = eyeBs[loopCheck(radio)];
+    rEyeB = eyeBs[loopCheck(radio)];
+    var radio = document.getElementsByName('s');
+    if (loopCheck(radio)===0){
+        neck = necks[0];
+        nJoin = nJoins[0];
+    }else if (loopCheck(radio)===1){
+        neck = necks[1];
+        nJoin = nJoins[0];
+    }else{
+        neck = necks[1];
+        nJoin = nJoins[1];
+    };
+    var radio = document.getElementsByName('a');
+    lArm = arms[loopCheck(radio)];
+    rArm = rArms[loopCheck(radio)];
+    var radio = document.getElementsByName('m');
+    mouth = mouths[loopCheck(radio)];
+    var radio = document.getElementsByName('f');
+    foot = feet[loopCheck(radio)];
+    m = gMonster();
+    document.getElementById('m').innerHTML = m;
+};
+
 function loopCheck(radio){
     for (var i = 0, length = radio.length; i < length; i++) {
         if (radio[i].checked) {
             return(i);
         };
     };
+};
+
+function sA(){
+    document.getElementById('easy').style.display='none';
+    document.getElementById('advanced').style.display='block';
+};
+
+function hA(){
+    document.getElementById('advanced').style.display='none';
+    document.getElementById('easy').style.display='block';
 };
